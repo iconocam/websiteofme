9b1307608170b371e9d9d98e1bd212a21b7f30c6
iconocam <150183122+iconocam@users.noreply.github.com>
2024-06-15 22:02:26 -0400

    implemented tailwind into portfolio, added navbar comp

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+return (
+    <div>
+        <h2>About Page</h2>
+        <p>This is Me</p>
+        </div>
+)
+}
+
+export default About
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+return (
+    <div>
+        <h2>Contact Me!</h2>
+        <p>What I do;</p>
+    </div>
+)
+}
+
+export default Contact
